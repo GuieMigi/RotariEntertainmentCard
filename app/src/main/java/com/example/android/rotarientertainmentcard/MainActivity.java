c314ab3b56874dd7e056c6235d83dfc10a250494
GuieMigi <florinchiriac2004@yahoo.com>
2018-02-17 20:49:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/rotarientertainmentcard/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.rotarientertainmentcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
